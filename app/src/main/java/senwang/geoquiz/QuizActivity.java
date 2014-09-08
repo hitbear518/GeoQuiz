87244c17f96fca4924dd013fd8aea6d5ea241b44
hitbear518@qq.com <sen@sen-ub.home>
2014-09-08 23:25:07 +0800

    97home

--- app/src/main/java/senwang/geoquiz/QuizActivity.java
+++ app/src/main/java/senwang/geoquiz/QuizActivity.java
@@ -1,5 +1,6 @@
 package senwang.geoquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.text.method.CharacterPickerDialog;
@@ -16,6 +17,7 @@ public class QuizActivity extends ActionBarActivity {
 
 	private Button mTrueButton;
 	private Button mFalseButton;
+    private Button mCheatButton;
     private Button mNextButton;
     private TextView mQuestionTextView;
 
@@ -28,6 +30,8 @@ public class QuizActivity extends ActionBarActivity {
     };
 
     private int mCurrentIndex = 0;
+
+    private boolean mIsCheater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +56,17 @@ public class QuizActivity extends ActionBarActivity {
 			}
 		});
 
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
+                startActivityForResult(i, 0);
+            }
+        });
+
         mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,7 +83,15 @@ public class QuizActivity extends ActionBarActivity {
         updateQuestion();
     }
 
-	@Override
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (data == null) {
+            return;
+        }
+        mIsCheater = data.getBooleanExtra(CheatActivity.EXTRA_ANSWER_SHOWN, false);
+    }
+
+    @Override
 	protected void onRestart() {
 		super.onRestart();
 		Log.d(Utils.getTag(), "onRestart() called");
@@ -121,10 +144,14 @@ public class QuizActivity extends ActionBarActivity {
 
         int messageResId;
 
-        if (userPressedTrue == answerIsTrue) {
-            messageResId = R.string.correct_toast;
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
         } else {
-            messageResId = R.string.incorrect_toast;
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
